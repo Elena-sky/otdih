a776e3bd7c7b832ad5bfb03cfc035bffd174d350
MacBook <alonasoroka1993@gmail.com>
2018-04-23 23:21:32 +0300

    commit

--- resources/views/template.blade.php
+++ resources/views/template.blade.php
@@ -20,6 +20,15 @@
 
     <link rel="stylesheet" href="{{ asset("css/font-awesome/css/font-awesome.min.css") }}">
 
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-118064580-1"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'UA-118064580-1');
+    </script>
 
 
 </head>
